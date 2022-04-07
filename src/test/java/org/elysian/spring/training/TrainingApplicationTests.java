a249bc629fec22591d9f1e8c39a2351f90264a15
Alexandru Sava <alexandru.sava@brivo.com>
2022-04-07 09:04:43 +0300

    feat: initial commit

---
+++ src/test/java/org/elysian/spring/training/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elysian.spring.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
