ba608b17ba32f5f2128248ca9e5d4f0d707c94d4
Alexandru Sava <alexandru.sava@brivo.com>
2022-04-07 16:15:55 +0300

    feat: add entities as described in Bogdan's Spring Boot Training Repository

---
+++ src/main/java/org/elysian/spring/training/model/AbstractEntity.java
@@ -0,0 +1,6 @@
+package org.elysian.spring.training.model;
+
+import java.io.Serializable;
+
+public abstract class AbstractEntity implements Serializable {
+}
---
+++ src/main/java/org/elysian/spring/training/model/Store.java
@@ -0,0 +1,90 @@
+package org.elysian.spring.training.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.SequenceGenerator;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity(name = "Store")
+public class Store {
+
+    @Id
+    @GeneratedValue(generator = "store_sequence_generator")
+    @SequenceGenerator(
+            name = "store_sequence_generator",
+            sequenceName = "store_sequence",
+            allocationSize = 1
+    )
+    private int id;
+
+    @Column(nullable = false, length = 50, insertable = true)
+    private String name;
+
+    @Column(nullable = false, length = 50, insertable = true)
+    private String location;
+
+    @OneToMany(mappedBy = "store", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Section> sections;
+
+    @PrePersist
+    public void beforeSave() {
+        System.out.println("Saving the Store " + getId());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(final String location) {
+        this.location = location;
+    }
+
+    public Set<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(Set<Section> sections) {
+        this.sections = sections;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Store)) {
+            return false;
+        }
+        Store store = (Store) o;
+        return id == store.id &&
+               Objects.equals(name, store.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
