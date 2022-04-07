48e015b4c4f814e569603a768f34ab008282cb7a
Alexandru Sava <alexandru.sava@brivo.com>
2022-04-08 00:54:47 +0300

    fix: forgot to add the import in the commit

--- src/main/java/org/elysian/spring/training/model/Product.java
+++ src/main/java/org/elysian/spring/training/model/Product.java
@@ -1,5 +1,7 @@
 package org.elysian.spring.training.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
