ecec4aed6e31f805a37a82bc837896d751c55add
Alexandru Sava <alexandru.sava@brivo.com>
2022-04-09 20:34:27 +0300

    feat: add one more constructor to Product model

--- src/main/java/org/elysian/spring/training/model/Product.java
+++ src/main/java/org/elysian/spring/training/model/Product.java
@@ -59,6 +59,12 @@ public class Product extends AbstractEntity {
         this.price = price;
     }
 
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     public int getId() {
         return id;
     }
